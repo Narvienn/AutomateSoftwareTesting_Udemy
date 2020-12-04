46d07f1d0cc8bc0056c6466e390e0acbbcb70304
zwachocka <zwachocka@andea.com>
2020-12-04 17:17:16 +0100

    First commit

---
+++ 54 Setting up an AT project.py
@@ -0,0 +1,13 @@
+# Step 1: install virtualenv (if not already installed)
+# Step 2: navigate to your selected folder and run: virtualenv venv --python=python3.6 (or higher)
+# (This is to separate the new virtualenv from any previous instances of it, and to avoid messing up library dependencies
+# e.g. if your old virtualenv uses older library versions that your previous projects rely on)
+
+# Step 3: run mkdir blog, then ls
+# blog becomes project folder and venv - the interpreter folder
+
+
+# Step 4: Under blog folder in your Python editor, create a tests folder
+# Step 5: Create Python files: app.py, blog.py, and post.py
+
+
